c5fc3e2e46b41ed8623133f2df2bbc89f9dc06b8
LOKESH YADAV <you@yourdomain.commyselflokeshyadav@gmail.com>
2017-08-19 02:52:07 +0530

    Code done

---
+++ test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by LOKESH YADAV on 19/08/17.
+//  Copyright © 2017 LOKESH YADAV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
